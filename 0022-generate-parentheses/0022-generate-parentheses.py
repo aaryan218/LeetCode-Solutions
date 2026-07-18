fb7767a238579a146017e1eba944f7970e6f6584
Aaryan Barthwal <aaryanbarthwal2187@gmail.com>
2026-07-18 09:55:24 +0530

    Time: 0 ms (100%), Space: 12.7 MB (18.83%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,17 +1,22 @@
 class Solution(object):
     def generateParenthesis(self, n):
-        result = []
-
-        def backtrack(current, open_count, close_count):
-            if len(current) == 2 * n:
-                result.append(current)
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        left, right = n, n
+        ans = []
+        def dfs(left, right, path):
+            if not left and not right:
+                ans.append(path)
                 return
+            if left :
+                # path += "("
+                dfs(left - 1, right, path + "(")
+            if right > left:
+                # path += ")"
+                dfs(left, right-1, path + ")")
 
-            if open_count < n:
-                backtrack(current + "(", open_count + 1, close_count)
+        dfs(left, right, "")
 
-            if close_count < open_count:
-                backtrack(current + ")", open_count, close_count + 1)
-
-        backtrack("", 0, 0)
-        return result
+        return ans
