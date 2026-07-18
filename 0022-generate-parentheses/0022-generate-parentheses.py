57a427fabd8741b6b957944074ea75a67456cffa
Aaryan Barthwal <aaryanbarthwal2187@gmail.com>
2026-07-18 09:55:01 +0530

    Time: 3 ms (41.92%), Space: 12.8 MB (18.83%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,17 @@
 class Solution(object):
     def generateParenthesis(self, n):
-        res = []
-        path = []
+        result = []
 
-        def bt(o, c):
-            if len(path) == 2 * n:
-                res.append("".join(path))
+        def backtrack(current, open_count, close_count):
+            if len(current) == 2 * n:
+                result.append(current)
                 return
 
-            if o < n:
-                path.append("(")
-                bt(o + 1, c)
-                path.pop()
+            if open_count < n:
+                backtrack(current + "(", open_count + 1, close_count)
 
-            if c < o:
-                path.append(")")
-                bt(o, c + 1)
-                path.pop()
+            if close_count < open_count:
+                backtrack(current + ")", open_count, close_count + 1)
 
-        bt(0, 0)
-        return res
+        backtrack("", 0, 0)
+        return result
