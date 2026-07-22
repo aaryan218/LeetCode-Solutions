c012698c476e1b4344b711abed25de4754a0108d
Aaryan Barthwal <aaryanbarthwal2187@gmail.com>
2026-07-22 21:31:10 +0530

    Time: 0 ms (100%), Space: 12.5 MB (69.77%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -11,6 +11,7 @@ class Solution(object):
         }
         stack = []
         for i in s:
+            
             if i in d:  
                 stack.append(i)
             elif len(stack) == 0 or d[stack.pop()] != i: 
